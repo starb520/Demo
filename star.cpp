33e6885519eb498afde7a3c9f88fc8fdb10e4288
starb520 <97459015+starb520@users.noreply.github.com>
2022-05-12 18:22:35 -0600

    I added a couple of comments

--- star.cpp
+++ star.cpp
@@ -11,7 +11,7 @@ Star::Star (const Point& ptUpperRight) :
    pt(ptUpperRight.getX() - random(20, 300), ptUpperRight.getY() - random(20, 300))
 
 {
-   phase = random(245, 255);
+   phase = random(245, 255);  // this is high so the brightness of the star is high, once its working we can change it back to (0,255)
    
 }
  
